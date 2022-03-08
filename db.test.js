fbf2347c342a47900b68409bb0e0a694ec9c6ea9
Bernard Mordan <bernardmordan@gmail.com>
2022-03-08 11:33:26 +0000

    updated CRUD tests

--- db.test.js
+++ db.test.js
@@ -1,25 +1,31 @@
 const db = require('better-sqlite3')(':memory:')
 
-describe("My DB", () => {
+describe('better-sqlite3 is used like this', () => {
     beforeAll(() => {
-        db.prepare('CREATE TABLE restaurants (id INTEGER PRIMARY KEY, name TEXT);').run();
-        const restaurantsInsert = db.prepare('INSERT INTO restaurants (name) VALUES (?)')
+        db.prepare('CREATE TABLE restaurants (id INTEGER PRIMARY KEY, name TEXT);').run()
+        const restaurantsInsert = db.prepare('INSERT INTO restaurants (name) VALUES (?);')
         restaurantsInsert.run('Bayroot')
         restaurantsInsert.run('Nandos')
     })
-    test("can connect OK", () => {
-        const rows = db.prepare('SELECT * FROM restaurants;').all()
-        expect(rows.length).toBe(2);
-        expect(rows[0].name).toBe('Bayroot')
+    test('can read out of a database', () => {
+        const restaurants = db.prepare('SELECT id, name FROM restaurants;').all()
+        expect(Array.isArray(restaurants)).toBeTruthy()
+        const [row1, row2] = restaurants
+        expect(row1.name).toBe('Bayroot')
+        expect(row2.id).toBe(2)
     })
-    test("can get a single item", () => {
-        const menu = db.prepare('SELECT * FROM menus WHERE id = ?').get(3)
-        expect(menu.title).toBe("Desserts")
+    test('we can update rows', () => {
+        const getRestaurant = db.prepare('SELECT id, name FROM restaurants WHERE id = ?;')
+        expect(getRestaurant.get(2).name).toBe('Nandos')
+        
+        const update = db.prepare('UPDATE restaurants SET name = ? WHERE id = ?;')
+        update.run('Cheeky Nandos', 2)
+
+        expect(getRestaurant.get(2).name).toBe('Cheeky Nandos')        
     })
-    test("we can create", () => {
-        const insertStatment = db.prepare('INSERT INTO menus (restaurant_id, title) VALUES (?, ?)')
-        insertStatment.run(1, 'Wine')
-        const newMenu = db.prepare('SELECT * FROM menus WHERE id = ?').get(4)
-        expect(newMenu.title).toBe('Wine')
+    test('we can also delete!', () => {
+        db.prepare('DELETE FROM restaurants WHERE id = 2;').run()
+        const restaurants = db.prepare('SELECT id, name FROM restaurants;').all()
+        expect(restaurants.length).toBe(1)
     })
 })
